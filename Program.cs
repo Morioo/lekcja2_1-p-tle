5ef5ea0408cb8d297ab7ee1901665a8529ca9e5a
Morioo <107799693+Morioo@users.noreply.github.com>
2024-09-03 13:45:32 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+    class Program
+    {
+        private static object? i;
+
+        static void Main(string[] args)
+        {
+
+        /*     Console.WriteLine("Różne rodzaje pętli");
+             //   for (int i = 0; i < 10000; i++) 
+             {
+
+                 //     Console.WriteLine("Wartość parametru i: {0}", i);
+
+
+             }
+             int i = 0;
+
+             for (; ; )
+             {
+
+                 if (i < 10)
+                 {
+
+                     Console.WriteLine("i = {0}", i);
+                     i++;
+                 }
+                 else
+                 {
+                     break;
+                 }
+
+                 //pętla nie skończona 
+
+                 for (; ; )
+
+                 {
+
+                     Console.WriteLine(1);
+
+                 }
+
+                 for (double d = 1.01d; d < 1.10; d += 0.01d) { 
+
+
+                     Console.WriteLine("d: {0:0.00}", d);
+
+                 }
+
+
+                 while (i<10 && i<=4)
+                 {
+                 Console.WriteLine("i = {0}", i);
+                 i++;
+
+
+                 }
+ */
+        /*       int i = 0, j=0;
+                   while (true)
+               {
+
+                   Console.WriteLine("ddddd");
+                   i++;
+                   if (i > 10)
+                       break;
+
+
+               }
+
+
+
+          //instr
+          i++;
+          while (j > 2)
+          {
+
+              j++;
+          }
+
+          */
+
+
+        // pętla do while
+        
+            //pętla foreach
+        
+
+
+            }
+
+        }
+
+
+
+
+
+
+    
+
